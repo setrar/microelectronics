31e3842b7c91aedf5a3b8554723c9d47a4729334
setrar <setrar@valiha.com>
2023-07-23 15:56:37 -0400

    .

--- lang/rust/tft_lcd/src/main.rs
+++ lang/rust/tft_lcd/src/main.rs
@@ -4,8 +4,9 @@
 use bsp::entry;
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    pac,
+    gpio, pac,
     sio::Sio,
+    spi::Spi,
     watchdog::Watchdog,
 };
 use defmt_rtt as _;
@@ -19,14 +20,14 @@ use embedded_hal::digital::v2::OutputPin;
 use embedded_graphics::{
     pixelcolor::Rgb565,
     prelude::*,
-    primitives::{Circle, Primitive, PrimitiveStyle, Rectangle, Triangle},
+    primitives::{Circle, Primitive, PrimitiveStyle, Triangle},
 };
 
 // Provides the parallel port and display interface builders
 use display_interface_spi::SPIInterfaceNoCS;
 
 // Provides the Display builder
-use mipidsi::Builder;
+use mipidsi::Builder as mipidsi;
 
 use fugit::RateExtU32;
 
@@ -78,7 +79,57 @@ fn main() -> ! {
 
     // Set the LED to be an output
     let mut led_pin = pins.led.into_push_pull_output();
+    
+    // Define the reset and write enable pins as digital outputs and make them high
+    let reset = pins
+        .gpio9
+        .into_push_pull_output_in_state(gpio::PinState::High);
+    let _wr = pins
+        .gpio5
+        .into_push_pull_output_in_state(gpio::PinState::High);
 
+    // Define the Data/Command select pin as a digital output
+    let dc = pins.gpio7.into_push_pull_output();
+    
+    // Define the SPI pins and create the SPI interface
+    let _sck = pins.gpio12;
+    let _miso = pins.gpio11;
+    let _mosi = pins.gpio13;
+    let _cs = pins.gpio10;
+
+    let spi = Spi::<_, _, 8>::new(pac.SPI0);
+//    let spi = spi.init(
+//        sck,
+//        mosi,
+//        miso,
+//        cs,
+//        100.kHz(),
+//        &embedded_hal::spi::MODE_0,
+//        clocks.peripheral_clock.freq(),
+//        &clocks,
+//   );
+    
+    let spi = spi.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        10.MHz(),
+        &embedded_hal::spi::MODE_0,
+        );
+        
+    // Define the display interface with no chip select
+    let di = SPIInterfaceNoCS::new(spi, dc);
+    
+    // Define the display from the display interface and initialize it
+    let mut display = mipidsi::ili9486_rgb565(di)
+        .init(&mut delay, Some(reset))
+        .unwrap();
+
+    // Make the display all black
+    display.clear(Rgb565::BLACK).unwrap();
+    
+    // Draw a smiley face with white eyes and a red mouth
+    draw_smiley(&mut display).unwrap();
+    
     // Blink the LED at 1 Hz
     loop {
         led_pin.set_high().unwrap();
@@ -86,6 +137,38 @@ fn main() -> ! {
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
+}
+
+fn draw_smiley<T: DrawTarget<Color = Rgb565>>(display: &mut T) -> Result<(), T::Error> {
+    // Draw the left eye as a circle located at (50, 100), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 100), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw the right eye as a circle located at (50, 200), with a diameter of 40, filled with white
+    Circle::new(Point::new(50, 200), 40)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+        .draw(display)?;
+
+    // Draw an upside down red triangle to represent a smiling mouth
+    Triangle::new(
+        Point::new(130, 140),
+        Point::new(130, 200),
+        Point::new(160, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
+    .draw(display)?;
+
+    // Cover the top part of the mouth with a black triangle so it looks closed instead of open
+    Triangle::new(
+        Point::new(130, 150),
+        Point::new(130, 190),
+        Point::new(150, 170),
+    )
+    .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
+    .draw(display)?;
+
+    Ok(())
 }
 
 // End of file
