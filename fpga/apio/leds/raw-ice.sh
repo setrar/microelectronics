c7117008b2aee1d87d8e6d8e1fb1e7e345167bb3
brobert <brobert@valiha.com>
2025-01-23 07:26:32 +0100

    .

--- fpga/apio/leds/raw-ice.sh
+++ fpga/apio/leds/raw-ice.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export SOURCE=${1}
+
 export ROOT=~/.icestudio/venv/bin/
 
 rm hardware.*
-${ROOT}apio raw 'yosys -p "synth_ice40 -top leds -json hardware.json" -q leds.v' &&
-${ROOT}apio raw 'nextpnr-ice40 --up5k --package sg48 --json hardware.json --asc hardware.asc --pcf leds.pcf -q' &&
+${ROOT}apio raw 'yosys -p "synth_ice40 -top leds -json hardware.json" -q ${SOURCE}.v' &&
+${ROOT}apio raw 'nextpnr-ice40 --up5k --package sg48 --json hardware.json --asc hardware.asc --pcf ${SOURCE}.pcf -q' &&
 ${ROOT}apio raw 'icepack hardware.asc hardware.bin'  &&
 dfu-util -d 1209:b1c0 -a 0 -D hardware.bin
 
